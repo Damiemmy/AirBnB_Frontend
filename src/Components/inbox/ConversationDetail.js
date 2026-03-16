136aca3127f247c84bd36ca5dbdea1045aa1d445
damiemmy <damisaemmanuel778@gmail.com>
2026-03-15 23:04:07 -0700

    feat: implement websocket client and connect frontend to backend for real-time messaging (integration debugging pending)

--- src/Components/inbox/ConversationDetail.js
+++ src/Components/inbox/ConversationDetail.js
@@ -1,8 +1,24 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import CustomButton from '../Forms/CustomButton'
+import useWebSocket from 'react-use-websocket'
 
-const ConversationDetail = () => {
+const ConversationDetail = ({conversation,userId,token}) => {
+  const myUser=conversation.users.find((user)=> user.id == userId)
+  const otherUser=conversation.users.find((user)=> user.id != userId)
+
+  const{sendJsonMessage,lastJsonMessage,readyState}= useWebSocket(`ws://127.0.0.1:8000/ws/${conversation.id}/?token=${token}`,
+    {
+      share: false,
+      shouldReconnect: () => True,
+    },
+  )
+
+  useEffect(()=>{
+    console.log('connection state changed',readyState);
+  },[readyState])
+
+
   return (
     <div className='max-h-[400px] overflow-auto flex flex-col space-y-4'>
       <div className='bg-gray-200 rounded-xl px-6 py-4 w-[80%]'>
--- src/app/inbox/[id]/page.js
+++ src/app/inbox/[id]/page.js
@@ -1,10 +1,26 @@
 import ConversationDetail from '@/Components/inbox/ConversationDetail'
 import React from 'react'
+import { getUserId } from '@/app/lib/action'
+import apiService from '@/services/ApiService'
+import { getAccessToken } from '@/app/lib/action'
 
-const page = () => {
+const page = async({params}) => {
+  const userId=await getUserId()
+  const token=await getAccessToken()
+    if(!userId){
+        return(
+            <main className="max-w-[1500px] max-auto px-6 py-12">
+                <p>You need to be Authenticated...</p>
+            </main>
+        )
+    }
+
+
+  const conversation=await apiService.get(`/api/chat/${params.id}`)
+
   return (
     <main className='max-w-[1500px] px-6 pb-6 mx-auto space-y-4'>
-      <ConversationDetail/>
+      <ConversationDetail conversation={conversation} userId={userId} token={token}/>
     </main>
   )
 }
