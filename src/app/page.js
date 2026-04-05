32dda6943be92b5924f16912e8fd981dc462c877
damiemmy <damisaemmanuel778@gmail.com>
2026-04-04 17:49:58 -0700

    fix: wrap homepage (/) useSearchParams in Suspense to fix Netlify build error

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Categories from '@/Components/Categories'
 import PropertyList from '@/Components/PropertyList/PropertyList'
+import { Suspense } from 'react'
 
 const page = () => {
   return (
     <div className='max-w-[1500px] mx-auto px-6'>
-      <Categories/>
-      <div className='mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6'>
-        <PropertyList/>
-      </div>
-      
+      <Suspense fallback={<p>Loading...</p>}>
+        <Categories/>
+        <div className='mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6'>
+          <PropertyList/>
+        </div>
+      </Suspense>
     </div>
   )
 }
